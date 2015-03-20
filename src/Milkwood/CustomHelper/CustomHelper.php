dc20d7c83ec80752f5e98f1290ca5fb04c054802
Philip Brechmann <pb@brechmann.dk>
2015-03-20 18:49:27 +0100

    Updated with new functions

--- src/Milkwood/CustomHelper/CustomHelper.php
+++ src/Milkwood/CustomHelper/CustomHelper.php
@@ -4,15 +4,13 @@
 
 	class CustomHelper{
 
-		public function slugify($stringToSlug){
+		public function getPercentFromTwoNumbers($number1,$number2,$decimalCount = 0){
 		
-			$stringToSlug = trim($stringToSlug);
-			$stringToSlug = strtolower($stringToSlug);
-			$stringToSlug = str_replace([' '], ['_'], $stringToSlug);
-			$stringToSlug = str_replace(['æ','ø','å'], ['ae','oe','aa'], $stringToSlug);
-			$stringToSlug = str_replace(['@','.'], ['aaa','-'], $stringToSlug);
-			return $stringToSlug;
-		
+			if($number2 == 0){
+				return 0 . "%";
+			}
+			
+			return $this->numberFormat($number1 / $number2 * 100, $decimalCount). '%';
 		}
 
 		public function getActionWithNamespace($action){
@@ -22,12 +20,52 @@
 		
 		}
 
+		public function danishFloatToFloat($danishFloat){
+		
+			return str_replace(',','.',$danishFloat);
+		
+		}
+
+		public function numberFormat($number,$decimalCount){
+			
+			return number_format( $number, $decimalCount, ',','.');
+		
+		}
+
 		// Add text in front of a string if the text is not found within the string
 		public function addTextToStringIfNotFound($needle,$haystack){
 		
 			return (strpos($haystack, $needle) !== false) ? $haystack : $needle . $haystack;
 		
 		}
+
+
+		public function average($totalValue,$count){
+			
+			if($count == 0 || $totalValue == 0){
+				return 0;
+			}
+			return $totalValue / $count;
+		
+		}
+
+		public function standardDeviation(array $a, $sample = false) {
+				$n = count($a);
+		        if( $n < 2){
+		        	return 0;
+		        }
+
+		        $mean = array_sum($a) / $n;
+		        $carry = 0.0;
+		        foreach ($a as $val) {
+		            $d = ((double) $val) - $mean;
+		            $carry += $d * $d;
+		        };
+		        if ($sample) {
+		           --$n;
+		        }
+		        return sqrt($carry / $n);
+		    }
 	}
 
 ?>
